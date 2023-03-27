0260d57acb788f4bac0ebf067c0ea1af07ebf3a1
sergdon <sergdon76@gmail.com>
2023-03-27 10:25:14 +0300

    init txt

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+    private static final Product[] goods = {
+            new Product("Кокос", 220.0),
+            new Product("Рис", 55.0),
+            new Product("Сахар", 61.0),
+            new Product("Кефир", 82.0),
+            new Product("Чай", 150.0),
+            new Product("Колбасный сыр", 240.0)
+    };
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        Basket shoppingCart = new Basket(goods);
+        int selectedItem;
+        int itemCount;
+
+        File basketFile = new File("/Users/macbookpro/IdeaProjects/Serialization2 task serial/basket.txt");
+
+        if (basketFile.exists()) {
+            System.out.println("Добро пожаловать!");
+            System.out.println("Желаете загрузить корзину покупок?  - нажмите <ENTER> ");
+            System.out.println();
+            if (scanner.nextLine().equals("")) {
+                Basket.loadFromTxtFile(basketFile);
+            }
+        }
+        while (true) {
+            shoppingCart.printGoodsList();
+            s = scanner.nextLine();
+            String[] inputValues = s.split(" ");
+            if (inputValues.length == 2) {
+                try {
+                    selectedItem = Integer.parseInt(inputValues[0]);
+                    itemCount = Integer.parseInt(inputValues[1]);
+
+                    if (selectedItem <= 0 || selectedItem > goods.length) {
+                        System.out.print("\nВнимание! Неправильный номер товара\n");
+                        continue;
+                    }
+                    if (itemCount <= 0) {
+                        continue;
+                    }
+                    shoppingCart.addToCart(selectedItem - 1, itemCount);
+
+                } catch (NumberFormatException nfe) {
+                    System.out.println("\nПожалуйста! Нужно для работы только 2 аргумента - 2 целых числа через пробел!");
+                }
+            } else if (s.equals("end")) {
+                break;
+            }
+            System.out.println("\nВнимание! Нужно для работы только 2 аргумента через пробел");
+        }
+        scanner.close();
+        shoppingCart.printCart();
+    }
+}
